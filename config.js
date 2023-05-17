3a8cf1180211dfd0e9176adc974951a0d65d801c
ANGJustinl <96008766+ANGJustinl@users.noreply.github.com>
2023-05-17 11:53:32 +0800

    statuspage

---
+++ config.js
@@ -0,0 +1,27 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'ANG Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2095842-877ee7be205a83ea1f3b4521',
+    //'ur2095842-b573189d8f939d2ff287f3be',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://www.angforever.top/'
+    },
+    
+  ],
+};
